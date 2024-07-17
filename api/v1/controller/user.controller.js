ad18225ccb1bc97baed8b98c4af8fec9f191661c
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-07-18 02:33:17 +0700

    Gửi mã OTP qua email

--- api/v1/controller/user.controller.js
+++ api/v1/controller/user.controller.js
@@ -1,7 +1,8 @@
 const User = require('../models/user.model')
+const ForgotPassword = require('../models/forgot-password.model')
 
-
-
+const generateHelper = require('../../../helpers/generate')
+const sendMailHelper = require('../../../helpers/sendEmail')
 
 
 const register = async (req , res) => {
@@ -63,8 +64,54 @@ const login = async (req, res) => {
     })
 
 }
+const forgotPassword = async (req, res) => {
+    try {
+        const email = req.body.email;
+        const user = await User.findOne({
+            email: email,
+            deleted: false
+        })
+        if(!user) {
+            res.json({
+                code : 404,
+                message : "Email không tồn tại!"
+            })
+            return;
+        }
+
+        const OTP = generateHelper.generateOTP(5);
+        const objectForgotPassword = {
+            email : email,
+            otp : OTP,
+            timeExpire : Date.now(),
+        }
+
+
+        // Lưu vào database
+        const forgotPassword = new Forgotpassword(objectForgotPassword)
+        await forgotPassword.save()
+
+        // Việc 2 : Gửi mã OTP qua email của user , vào trang để nhập mã otp
+        const subject = "Xác Thực OTP"
+        const html = `Xin chào ${user.fullname} <br> Mã xác thực OTP là:</br>  <b>${objectForgotPassword.otp} </b>`
+        
+        sendMailHelper.sendEmail(email,subject,html);
+
+        res.json({
+            code : 200,
+            message : "Success!",
+        })
+    } catch (error) {
+        res.json({
+            code : 200,
+            message : "Error!",
+            error : error
+        })
+    } 
+}
 
 module.exports = {
     register,
     login,
+    forgotPassword,
 }
