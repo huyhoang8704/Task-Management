6193b3a8747160b579fe5f0cae22a73e349e8d28
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-08-07 10:24:26 +0700

    Pagination

--- task-management-ts/api/v1/controller/task.controller.ts
+++ task-management-ts/api/v1/controller/task.controller.ts
@@ -1,6 +1,6 @@
 import { Express, Request, Response , Router } from "express"
 import Task from "../models/task.model";
-
+import paginationHelpers from "../../../helpers/pagination";
 
 
 const index  = async(req : Request, res : Response) => {
@@ -13,10 +13,29 @@ const index  = async(req : Request, res : Response) => {
         const sortKey = req.query.sortKey.toString();
         sort[sortKey] = req.query.sortValue
     }
-    
-    const tasks = await Task.find(find).sort(sort);
+    // Pagination : http://localhost:3000/api/v1/tasks?page=1
+    const countTasks = await Task.countDocuments(find);
+    let objectPagination = paginationHelpers(
+        {
+            currentPage : 1 ,
+            limitItem : 20
+        },
+        req.query,
+        countTasks
+    )
+
+
+
+    const tasks = await Task
+            .find(find)
+            .sort(sort)
+            .limit(objectPagination.limitItem)  // Lấy ra số sản phẩm trên 1 trang
+            .skip((objectPagination.currentPage-1)* objectPagination.limitItem) // pagination
     res.json(tasks)
 }
+
+
+
 const detail = async(req : Request, res : Response) => {
     const task = await Task.findOne({
         _id: req.params.id,
---
+++ task-management-ts/helpers/pagination.ts
@@ -0,0 +1,10 @@
+
+const paginationHelpers = (objectPagination, query, countTasks) => {
+    if(query.page) objectPagination.currentPage = parseInt(query.page) // lấy trang hiện tại trên query
+
+    const totalPage = Math.ceil(countTasks / objectPagination.limitItem)
+    objectPagination.totalPage = totalPage 
+    return objectPagination
+}
+
+export default paginationHelpers;
