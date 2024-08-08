d13756bed75f96f6aa2744e0a9fbd55089a5723c
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-08-08 10:49:20 +0700

    Deploy on vercel typescript

--- task-management-ts/api/v1/routes/index.route.ts
+++ task-management-ts/api/v1/routes/index.route.ts
@@ -1,12 +1,12 @@
 import { taskRoute } from "./task.route"
 import { Express } from "express";
 import { userRoute } from "./user.route";
-
+import * as authMiddleware from "../middlewares/auth.middleware"
 
 const mainV1Routes = (app : Express) : void => {
     const PATHversion1 = "/api/v1"
 
-    app.use(PATHversion1 + "/tasks",taskRoute);
+    app.use(PATHversion1 + "/tasks",authMiddleware.requireAuth,taskRoute);
 
     app.use(PATHversion1 + "/user",userRoute);
 
